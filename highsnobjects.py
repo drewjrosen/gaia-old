54b7049ac9e7ad0a7e79813b3a31fc670872c89e
drewjrosen <drew.j.rosen@gmail.com>
2017-03-22 13:01:45 -0400

    Added H-R diagram plotting

--- highsnobjects.py
+++ highsnobjects.py
@@ -1,13 +1,18 @@
+import math
 import astropy
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits
 from mpl_toolkits.mplot3d import Axes3D
-import sys, os, time, string, math, commands, subprocess
+import sys, os, time, string, commands, subprocess
 
 #read in data from Gaia catalog
 file=fits.open('tgas-source.fits')
 data_list=file[1]
+
+#read in data from 2mass
+file2=fits.open('tgas-matched-2mass.fits')
+data_list_2=file2[1]
 
 
 #Pull data we want from .fits file
@@ -25,6 +30,12 @@ ra=data_list.data['ra']
 dec=data_list.data['dec']
 
 
+#magnitudes
+g_band=data_list.data['phot_g_mean_mag']
+j_band=data_list_2.data['j_mag']
+k_band=data_list_2.data['k_mag']
+h_band=data_list_2.data['h_mag']
+
 
 #calculate SN ratio
 ratio=parallax/parallax_error
@@ -39,30 +50,53 @@ highSNindices = ratio > 16.
 distance=1./parallax[highSNindices] #in Kpc
 
 #Calculate transverse velocity in RA and DEC then in real space
-transv_ra=4.74*propermotion_ra[highSNindices]*distance #km/s
-transv_dec=4.74*propermotion_dec[highSNindices]*distance #km/s
+#transv_ra=4.74*propermotion_ra[highSNindices]*distance #km/s
+#transv_dec=4.74*propermotion_dec[highSNindices]*distance #km/s
 
-transverse_vsqared=transv_ra**2+transv_dec**2
-transverse_v=transverse_vsqared**(.5)
+#transverse_vsqared=transv_ra**2+transv_dec**2
+#transverse_v=transverse_vsqared**(.5)
 
 
-#test add
+
+
+#calcilate color G-J
+j_k=j_band[highSNindices]-k_band[highSNindices]
+
+#calculate absolute magnitude
+distance_pc=distance*10**3.
+
+
+absolute_mag=g_band[highSNindices]-(5.*(np.log10(distance_pc)-1))
 
 #plotting
 
-#distance distribution
-plt.hist(distance,bins=100,log=True)
+#H-R diagram
+
+
+#make figure
+rect1=0.1,0.1,0.75,0.75
+fig1=plt.figure(1)
+ax1=fig1.add_axes(rect1)
+
+ax1.plot(j_k,absolute_mag, "o")
+ax1.invert_yaxis()
+ax1.set_ylabel("Absolute Magnitude, G")
+ax1.set_xlabel("J-K")
 plt.show()
+
+#distance distribution
+#plt.hist(distance,bins=100,log=True)
+#plt.show()
 
 #Relative velocity distribution
-plt.hist(transverse_v,bins=100,log=True)
-plt.show()
-plt.savefig('highsnVelocities.png')
+#plt.hist(transverse_v,bins=100,log=True)
+#plt.show()
+#plt.savefig('highsnVelocities.png')
 
 #3D Map of selected stars
-fig=plt.figure()
-ax=fig.add_subplot(111,projection='3d')
-ax.scatter(ra[highSNindices],dec[highSNindices],distance)
-plt.show()
-plt.savefig('highsn3Dmap.png')
+#fig=plt.figure()
+#ax=fig.add_subplot(111,projection='3d')
+#ax.scatter(ra[highSNindices],dec[highSNindices],distance)
+#plt.show()
+#plt.savefig('highsn3Dmap.png')
 
